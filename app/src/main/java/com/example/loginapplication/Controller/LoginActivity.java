7a4ef56fd99f305a15d8f131772379fa4e68086d
YakovShechter <ycsycs1@gmail.com>
2017-06-14 20:16:48 +0300

    Reoriginize3

--- app/src/main/java/com/example/loginapplication/Controller/LoginActivity.java
+++ app/src/main/java/com/example/loginapplication/Controller/LoginActivity.java
@@ -47,21 +47,12 @@ import static android.Manifest.permission.READ_CONTACTS;
  */
 public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
 
-    /**
-     * Id to identity READ_CONTACTS permission request.
-     */
     private static final int REQUEST_READ_CONTACTS = 0;
 
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
+    // TODO: remove after connecting to a real authentication system.
     private static final String[] DUMMY_CREDENTIALS = new String[]{
             "foo@example.com:hello", "bar@example.com:world"
     };
-    /**
-     * Keep track of the login task to ensure we can cancel it if requested.
-     */
     private UserLoginTask mAuthTask = null;
 
     // UI references.startActivity(new Intent(LoginActivity.this, BusinessActivity.class));
@@ -72,7 +63,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
 
         // Check if any user logged in
         if (SaveSharedPreference.getUserName(LoginActivity.this).length() == 0) {
